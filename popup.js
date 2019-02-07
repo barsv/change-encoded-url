09ceee429a0ecec5a0eb84bbd5c88786bc66994f
stanislav.baranov <stan@utilli.com>
2019-02-08 02:01:49 +0400

    added copy button

--- popup.js
+++ popup.js
@@ -6,10 +6,15 @@ go.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var url = document.getElementById('url').value;
         chrome.tabs.update(
-             tabs[0].id,
-             { url: url });
+            tabs[0].id,
+            { url: url });
         window.close();
     });
+};
+
+document.getElementById('copy').onclick = function () {
+    var url = document.getElementById('url').value;
+    sendToClipboard(url);
 };
 
 document.getElementById('url').onkeypress = function () {
@@ -23,6 +28,18 @@ document.getElementById('url').onkeypress = function () {
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var decodedUrl = decodeURI(tabs[0].url)
     document.getElementById('url').value = decodedUrl;
-    console.log(15*(decodedUrl.length / 94) + 15);
-    document.getElementById('url').style.height = (15*(decodedUrl.length / 94) + 15) + 'px';
+    console.log(15 * (decodedUrl.length / 94) + 15);
+    document.getElementById('url').style.height = (15 * (decodedUrl.length / 94) + 15) + 'px';
 });
+
+function sendToClipboard(myString) {
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+
+    input.value = myString;
+    input.focus();
+    input.select();
+
+    document.execCommand('Copy');
+    input.remove();
+}
